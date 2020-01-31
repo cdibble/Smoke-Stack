0e698fdd212d689273d5863c5c6a63e510ecddd4
cdibble <cddibble@gmail.com>
2020-01-30 19:52:08 -0800

    Updated functions: extract_gdb_to_parquet_functions.py includes draft functions. pySpark_DB_Extract.py is a spark-shell ready extraction routine that reads csv files from an s3 bucket, does some timedate munging, and writes them to a parquet file.

--- ingestion/extract_gdb_to_parquet_functions.py
+++ ingestion/extract_gdb_to_parquet_functions.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-# Install dependencies:
-#1. install gdal :: https://gis.stackexchange.com/questions/28966/python-gdal-package-missing-header-file-when-installing-via-pip/74060#74060?newreg=ff051bdd59f8443d954e7ef5f5f4a5e7
-sudo apt-get install gdal-bin
-sudo apt-get install libgdal-dev
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-export C_INCLUDE_PATH=/usr/include/gdal
-pip3 install GDAL==2.2.3 # get version using gdalinfo --version
-
 # Imports:
 from osgeo import gdal
 from osgeo import ogr
-from osgeo import osr
-from osgeo import gdal_array
-from osgeo import gdalconst
+# from osgeo import osr
+# from osgeo import gdal_array
+# from osgeo import gdalconst
 import pandas as pd
 import datetime
+from pyspark.sql import functions as psql
 gdal.UseExceptions() # raise exceptions instead of returning errors
-gdb_path = "/Users/Connor/Documents/InisightDE/1_Project/Data/01_January_2009/Zone1_2009_01.gdb"
-gdb_path = 's3://ais-ship-data-rawData/2010/09_September_2010/Zone4_2010_09/Zone4_2010_09.gdb'
 
+
+# import com.esri.gdb._
+# import com.esri.udt._
+gdb_path = "/Users/Connor/Documents/InisightDE/1_Project/Data/01_January_2009/Zone1_2009_01.gdb"
+gdb_path = 's3a://ais-ship-data-raw/Data/2010/09_September_2010/Zone4_2010_09/Zone4_2010_09.gdb'
+import os
+sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", os.environ["AWS_ACCESS_KEY_ID"])
+sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", os.environ["AWS_SECRET_ACCESS_KEY"])
+sc._jsc.hadoopConfiguration().set("fs.s3a.region", os.environ["AWS_REGION"])
 
 # TODO: 
 # Convert pandas manipulations to sparkDF
@@ -29,7 +29,19 @@ gdb_path = 's3://ais-ship-data-rawData/2010/09_September_2010/Zone4_2010_09/Zone
 # parse file structure and iterate over files
 
 # Build function for importing attributes, clipping files.
-def parse_gdb(gdb_path, out_path=None):
+	# TODO: Don't iterate over unwanted layers.
+def parse_gdb_local(gdb_path, out_path=None):
+	import os
+	import boto3
+	client = boto3.client('s3')
+	os.makedirs(gdb_path, exist_ok=True) # create directory strcture for download.
+	files_to_fetch_i = list(ais_bucket.objects.filter(Prefix = gdb_path)) # get folder_i
+	files_to_fetch_i = [files_to_fetch_i[x].key for x in range(len(files_to_fetch_i))] # get keys only
+	client.download_file('ais-ship-data-raw', \
+			# gdb_folders[i] + '/' + str(os.path.basename(files_to_fetch[j])), \
+			str(files_to_fetch[j]), \
+			str(files_to_fetch[j]) )
+
 	driver = ogr.GetDriverByName("OpenFileGDB") # get appropriate driver.
 	ds = driver.Open(gdb_path, 0) # open GDB file; 0 is for read-only, 1 is for writeable.
 	numLayers = ds.GetLayerCount() # Find number of layers in file.
@@ -88,12 +100,112 @@ def parse_gdb(gdb_path, out_path=None):
 					print("ERROR: No Geometry For This Vessel Feature")
 	return feature_list, vessel_feature_list
 
+
+## Version 2: Using FileGDB (https://github.com/mraad/FileGDB)
+# TODO, add verbose option to print schema and tables
+def parse_FileGDB(gdb_path, out_path=None):
+	jvm = spark._jvm # get ref to underlying JVM.
+	gdb = jvm.com.esri.gdb.FileGDB # get ref to FileGDB Scala object
+	# gdb.listTables(gdb_path) # List all tables in gdb.
+	# schema_Broadcast = gdb.schema(gdb_path,'Broadcast') # get schema for 'Broadcast'
+	# schema_Vessel = gdb.schema(gdb_path,'Vessel') # get schema for 'Vessel'
+	## Create Spark DF from GDB
+	# Needs credentials
+	## Ping Table
+	ping_table = spark.read \
+		.format("com.esri.gdb") \
+		.options(path="s3a://ais-ship-data-raw/" + gdb_path, name="Broadcast") \
+		.load()
+	# ping_table.registerTempTable("Broadcast") # register table for upcoming SQL ops; not sure what this means.
+	# Use sql (pyspark.sql, i think) commands to map pings to evenly spaced cells, then get the lat/lon of the cells
+		# Note - the cells are based on the lat/long ranges in this shard only, so are not meaningful overall
+		# cell_1 = 0.001
+		# cell_2 = cell_1 * 0.5
+		# sql(f"select cast(floor(Shape.x/{cell_1}) as INT) as q,cast(floor(Shape.y/{cell_1}) as INT) as r from Broadcast")\
+		# 	.registerTempTable("QR")
+		# sql(f"select q*{cell_1}+{cell_2} as x,r*{cell_1}+{cell_2} as y,count(1) as pop from QR group by q,r having pop > 100").show()
+	# make datetime objects, extract Year and Month.
+	ping_table.select(to_timestamp('BaseDateTime', '%Y/%m/%d %H:%M:%S')).alias('BaseDateTime')
+	ping_table.select(to_timestamp('BaseDateTime', '%Y/%m/%d %H:%M:%S')).alias('Year') # need to extract %Y and convert to string 
+	ping_table.select(to_timestamp('BaseDateTime', '%Y/%m/%d %H:%M:%S')).alias('Month') # need to extract %m and convert to string 
+	## Vessel Table
+	vessel_table = spark.read \
+		.format("com.esri.gdb") \
+		.options(path=gdb_path, name="Vessel") \
+		.load()
+	# vessel_table.registerTempTable("Vessel") # register table for upcoming SQL ops; not sure what this means.
+	return ping_table, vessel_table
+
 # Test parse_gdb()
 # feature_list, vessel_feature_list = parse_gdb(gdb_path)
-rdd = sc.hadoopFile('s3n://my_bucket/my_file', conf = {
-  'fs.s3n.awsAccessKeyId': '...',
-  'fs.s3n.awsSecretAccessKey': '...',
-})
+
+# manipulating timestamps with pyspark.sql
+# https://stackoverflow.com/questions/57642177/extracting-the-year-from-date-in-pyspark-dataframe
+
+def parse_csv(csv_file, out_path=None):
+	from pyspark.sql import functions as psql	# Not sure what these files actually look like (Broadcast vs. Vessel tables).... Need to look first.
+	# sqlContext = SQLContext(sc)
+	csv_i = spark.read.format("csv").option("inferSchema", \
+		True).option("header", True).load("s3a://ais-ship-data-raw/" + csv_file)
+	csv_i = csv_i.withColumn("Year", psql.year('BaseDateTime'))
+	csv_i = csv_i.withColumn("Month", psql.month('BaseDateTime'))
+	return(csv_i)
+
+	
+# Test
+parse_csv(csv_files[0])
+sc.textFile("s3://ais-ship-data-raw" + csv_files[0]).take(1)
+sc.readcsv("s3://ais-ship-data-raw" + csv_files[0]).take(1)
+
+
+
+
+
+#####################################################
+	# Create Spark DF from GDB
+	## Ping Table
+	ping_table = spark.read \
+		.format("com.esri.gdb") \
+		.options(path=gdb_path, name="Broadcast") \
+		.load()
+	# ping_table.registerTempTable("Broadcast") # register table for upcoming SQL ops; not sure what this means.
+	# Use sql (pyspark.sql, i think) commands to map pings to evenly spaced cells, then get the lat/lon of the cells
+		# Note - the cells are based on the lat/long ranges in this shard only, so are not meaningful overall
+		# cell_1 = 0.001
+		# cell_2 = cell_1 * 0.5
+		# sql(f"select cast(floor(Shape.x/{cell_1}) as INT) as q,cast(floor(Shape.y/{cell_1}) as INT) as r from Broadcast")\
+		# 	.registerTempTable("QR")
+		# sql(f"select q*{cell_1}+{cell_2} as x,r*{cell_1}+{cell_2} as y,count(1) as pop from QR group by q,r having pop > 100").show()
+	ping_table.select(to_timestamp('BaseDateTime', '%Y/%m/%d %H:%M:%S')).alias('BaseDateTime')
+	ping_table.select(to_timestamp('BaseDateTime', '%Y/%m/%d %H:%M:%S')).alias('Year') # need to extract %Y and convert to string 
+	ping_table.select(to_timestamp('BaseDateTime', '%Y/%m/%d %H:%M:%S')).alias('Month') # need to extract %m and convert to string 
+	## Vessel Table
+	vessel_table = spark.read \
+		.format("com.esri.gdb") \
+		.options(path=gdb_path, name="Vessel") \
+		.load()
+	# vessel_table.registerTempTable("Vessel") # register table for upcoming SQL ops; not sure what this means.
+	return ping_table, vessel_table
+
+# pandas: read_json, to_parquet
+def write_pings_parquet_to_s3(object_to_write = None, bucket_dir = "s3a://ais-ship-pings-parquet/", file_name = None):
+	spark_object_to_write = spark.createDataFrame(object_to_write) # converts pandas df to spark RDD with @F.pandas_udf('data_class', F.PandasUDFType.SCALAR)
+	spark_object_to_write.write.mode("append").format('parquet').partitionBy('Year', 'Month').option('compression', 'snappy').save(bucket_dir + file_name)
+
+def write_vessels_parquet_to_s3(object_to_write = None, bucket_dir = "s3a://ais-ship-vessel-info-parquet/", file_name = None):
+	spark_object_to_write = spark.createDataFrame(object_to_write) # converts pandas df to spark RDD with @F.pandas_udf('data_class', F.PandasUDFType.SCALAR)
+	spark_object_to_write.write.mode("append").format('parquet').option('compression', 'snappy').save(bucket_dir + file_name)
+
+def write_csv_parquet_to_s3(object_to_write = None, bucket_dir = "s3a://ais-ship-pings-parquet/", file_name = None):
+	# spark_object_to_write = spark.createDataFrame(object_to_write) # converts pandas df to spark RDD with @F.pandas_udf('data_class', F.PandasUDFType.SCALAR)
+	object_to_write.write.mode("append").format('parquet').option('compression', 'snappy').save(bucket_dir + file_name)
+
+# Test write_parquet_to_s3
+bucket_dir = "s3://ais-ship-pings-parquet/Data/"  # S3 Path need to mention
+file_name = "ais-pings.parquet"
+write_parquet_to_s3(feature_list, bucket_dir, file_name)
+
+# ========================================================================
 # Get contents of AttributeUnits layer once to check it out. current_layer defined for i = 4
 # [current_layer.GetFeature(x).ExportToJson() for x in range(1,features_in_layer+1)]
 # OUTPUT: 
@@ -113,13 +225,38 @@ rdd = sc.hadoopFile('s3n://my_bucket/my_file', conf = {
 	# {"Attribute": "DimensionComponents", "Unit": "Meters"}, "id": 7}', '
 	# {"type": "Feature", "geometry": null, "properties": 
 	# {"Attribute": "Draught", "Unit": "1/10 Meters"}, "id": 8}']
+# ========================================================================
+# DEV:
+# get_object and download_file methods for s3 objects
+# https://stackoverflow.com/questions/37442444/download-s3-files-with-boto
+client = boto3.client('s3')
 
-# pandas: read_json, to_parquet
-def write_parquet_to_s3(object_to_write, bucket_dir, file_name):
-	spark_object_to_write = spark.createDataFrame(object_to_write)
-	spark_object_to_write.write.mode("append").format('parquet').option('compression', 'snappy').save(bucket_dir + file_name)
+# if your bucket name is mybucket and the file path is test/abc.txt
+# then the Bucket='mybucket' Prefix='test'
 
-# Tetst write_parquet_to_s3
-bucket_dir = "s3://ais-ship-pings-parquet/Data/"  # S3 Path need to mention
-file_name = "ais-pings.parquet"
-write_parquet_to_s3(feature_list, bucket_dir, file_name)
+resp = client.list_objects_v2(Bucket="<your bucket name>", Prefix="<prefix of the s3 folder>") 
+
+for obj in resp['Contents']:
+	key = obj['Key']
+	#to read s3 file contents as String
+	response = client.get_object(Bucket="<your bucket name>",
+						 Key=key)
+	print(response['Body'].read().decode('utf-8'))
+
+
+
+
+##### For Testing FileGDB package from github
+jvm = spark._jvm # get ref to underlying JVM.
+gdb = jvm.com.esri.gdb.FileGDB # get ref to FileGDB Scala object
+gdb_path = 'Data/2011/05/Zone14_2011_05.gdb/Zone14_2011_05.gdb' # for testing
+gdb_path = "s3a://ais-ship-data-raw/" + gdb_path
+ping_table = spark.read \
+		.format("com.esri.gdb") \
+		.options(path=gdb_path, name="Broadcast") \
+		.load()
+
+ping_table = sc.textFile('s3a://ais-ship-data-raw/' + gdb_path) \
+		.format("com.esri.gdb") \
+		.options(path="s3a://ais-ship-data-raw/" + gdb_path, name="Broadcast") \
+		.load()			
--- ingestion/pySpark_DB_Extract.py
+++ ingestion/pySpark_DB_Extract.py
@@ -1,84 +1,107 @@
 #!/usr/bin/env python3
-# ===========================================================================
-# Python3 script
-# geospatial modules
-from osgeo import gdal
-from osgeo import ogr
-from osgeo import osr
-from osgeo import gdal_array
-from osgeo import gdalconst
-# import other modules:
-import pandas as pd
-# import numpy as np
-from functools import reduce
-import pyspark #pyspark.sql , pyspark.ml
+# Imports
+from pyspark import SparkContext, SparkConf, SQLContext
+from pyspark.sql import functions as psql
+from pyspark.sql.types import *
 import boto3 
 import botocore
 import datetime
 import os
+import timeit
+import time
+# import pandas as pd
+# import numpy as np
+# from functools import reduce
+# import sys
+# sys.path.append('~/Scripts')
+# import extract_gdb_to_parquet_functions as EF
+# module, inputFile1, outputDir1 = sys.argv
 
-import sys
-sys.path.append('./src')
-import as sf
 	# If running as spark-submit script.
-# conf = SparkConf().setAppName("AIS_Extract").setMaster(master)
-# sc = SparkContext(conf=conf)
-	# If running as pyspark shell
-# launch pyspark shell
-# ./usr/local/spark/bin/pyspark
+conf = SparkConf().setAppName("AIS_Extract") #.setMaster(spark://10.0.0.7:7077)
+# sc = SparkContext(conf=conf, pyFiles = ["/home/ubuntu/Scripts/extract_gdb_functions.py"] ) # to pass scprits to spark context
+sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+# spark = SparkSession.builder.appName("AIS_Extract") # https://github.com/carolsong/tonebnb/blob/master/batching/util.py
+# spark = psql.builder.appName("AIS_Extract").getOrCreate() # I don't think this is necessary if you use SparkConf().
 
-# 1. initiate boto3 resource & get folders with GDB databases
-s3 = boto3.resource('s3')
-# for bucket in s3.buckets.all(): # print bucket names
-# 	print(bucket.name)
+###### Functions to Parse Files
+def parse_csv(spark):
+	""" Function : Reads CSV files from a source S3 bucket and writes to a target S3 bucket in Parquet
+		:type spark: A spark session.
+	"""
+	source_bucket_dir = "s3a://ais-ship-data-raw/"
+	source_file_name = "*/*/*/*/*/*.csv"
+	# source_file_name = csv_files[0] # For Testing
+	# TODO: list matched files for QA
+	schema = StructType([
+	StructField('MMSI',StringType(),True),
+	StructField('BaseDateTime',StringType(),True),
+	StructField('LAT',StringType(),True),
+	StructField('LON',StringType(),True),
+	StructField('SOG',StringType(),True),
+	StructField('COG',StringType(),True),
+	StructField('Heading',StringType(),True),
+	StructField('VesselName',StringType(),True),
+	StructField('IMO',StringType(),True),
+	StructField('CallSign',StringType(),True),
+	StructField('VesselType',StringType(),True),
+	StructField('Status',StringType(),True),
+	StructField('Length',StringType(),True),
+	StructField('Width',StringType(),True),
+	StructField('Draft',StringType(),True),
+	StructField('Cargo',StringType(),True)
+	])
+	try:
+		# Read CSVs, add partition columns
+		# tic = time.clock()
+		# csv_i = sqlContext.read.csv(source_bucket_dir + source_file_name, schema = schema, header = True)
+		# tok = time.clock()
+		# tok - tic
+		# csv_i = sqlContext.read.format("csv").option(schema = schema).option("header", True).load(source_bucket_dir + source_file_name)
+		csv_i = sqlContext.read.csv(source_bucket_dir + source_file_name, schema = schema, header = True) # Fastest by an order of mag
+		# csv_i = sqlContext.read.format("csv").option("header", True).load(source_bucket_dir + source_file_name).rdd
+		# csv_i = sc.textFile(source_bucket_dir + source_file_name).map(lambda line: (line.split(',')[0], line.split(',')[1])).collect()
+		csv_i = csv_i.withColumn("BaseDateTime", psql.to_timestamp('BaseDateTime'))
+		csv_i = csv_i.withColumn("Year", psql.year('BaseDateTime'))
+		csv_i = csv_i.withColumn("Month", psql.month('BaseDateTime'))
+	except:
+		print("ERROR")
+	return(csv_i)
+
+def write_csv_to_parquet(spark, csv_i):
+		# Write Parquet to S3 
+		target_bucket_dir = "s3a://ais-ship-pings-parquet/"
+		target_file_name = 'pings.parquet'
+		csv_i.write.mode("append").format('parquet').partitionBy('Year', 'Month').option('compression', 'snappy').save(target_bucket_dir + target_file_name)
+		# csv_i.write.mode("append").format('parquet').option('compression', 'snappy').save(target_bucket_dir + target_file_name)
+# Script to get files/folders for parsing and call functions
+## 1. initiate boto3 resource & get folders with GDB databases
+s3 = boto3.resource('s3') # TODO: Initialize is expensive. Can I do all of the client. operations below with this resrouce?
 ais_bucket = s3.Bucket('ais-ship-data-raw') # define the bucket
 files_in_bucket = list(ais_bucket.objects.all()) # list all objects in the bucket
-# iterate and remove any portion of the key beyond the last '/'
+# Find .gdb folders: iterate and remove any portion of the key beyond the last '/'
 paths_in_bucket = [os.path.dirname(files_in_bucket[x].key) for x in range(len(files_in_bucket))] # return just the file paths but not the files names
 all_folders = set(paths_in_bucket) # unique parent 'directories'
 gdb_folders = [i for i in list(all_folders) if 'gdb' in i] # subset to those with gdb files
-gdb_folders_spark  = sc.parallelize(list(gdb_folders))
-# Find CSV files
+# gdb_folders = ['s3://ais-ship-data-raw/' + i for i in gdb_folders]
+gdb_folders_spark  = sc.parallelize(list(gdb_folders[0:2])) # create spark RDD from list
+# Find CSV files:
 keys_in_bucket = [files_in_bucket[x].key for x in range(len(files_in_bucket))] # get all bucket object keys
 csv_files = [i for i in keys_in_bucket if 'csv' in i] # find all csv files
+# csv_files_spark = sc.parallelize(csv_files[0:10]) # create spark RDD from list
 
-# 2. iterate on the folders to run parse_gdb and write_parquet_to_s3
-client = boto3.client('s3')
-# Download all of the files 
-for i in range(len(gdb_folders)):
-	files_to_fetch_i = list(ais_bucket.objects.filter(Prefix = gdb_folders[i]))
-	files_to_fetch_i = [files_to_fetch_i[x].key for x in range(len(files_to_fetch_i))] # get keys only
-	for j in range(len(files_to_fetch)):
-		# create directory for storing the files
-		os.makedirs(gdb_folders[i], exist_ok=True)
-		client.download_file('ais-ship-data-raw', \
-			# gdb_folders[i] + '/' + str(os.path.basename(files_to_fetch[j])), \
-			str(files_to_fetch[j]), \
-			str(files_to_fetch[j]))
-			# gdb_folders[i] + '/' + str(os.path.basename(files_to_fetch[j])))
-	local_path = gdb_folders[i]
-	# Submit spark job.
-	pings, vessels = parse_gdb(local_path)
-# 
+# 2. Parse CSVs
+# csv_tables = csv_files_spark.map(lambda x: write_csv_parquet_to_s3(parse_csv(x, bucket_in = 'ais-ship-data-raw').collect(), file_name = 'pings_from_csv.parquet'))
+# csv_i = sqlContext.read.format("csv").option("inferSchema", \
+# 		True).option("header", True).load("s3a://ais-ship-data-raw/*")
 
-# ==========================================================================================
-# Notes and rejects:
-client.download_fileobj('ais-ship-data-raw', gdb_folders[0], os.path.basename(files_to_fetch[0]))
+csv_i = parse_csv(sc)
+write_csv_to_parquet(sc, csv_i)
 
-# Use for csv 
-# need exception for reading csv's because some folders are parent directories and won't actually return anything.
-spark.read.csv()
+# 3. TODO Parse GDBs
+# gdb_tables = gdb_folders_spark.map(lambda x: parse_gdb(x, bucket_in = 'ais-ship-data-raw'))
+# print(gdb_tables)
+# print(gdb_tables.first())
 
-# Convert from pandas df to spark df
-# https://docs.databricks.com/spark/latest/spark-sql/spark-pandas.html
-# Enable Arrow-based columnar data transfers
-spark.conf.set("spark.sql.execution.arrow.enabled", "true")
-# Generate a pandas DataFrame
-pdf = pd.DataFrame(np.random.rand(100, 3))
-# Create a Spark DataFrame from a pandas DataFrame using Arrow
-df = spark.createDataFrame(pdf)
-# Convert the Spark DataFrame back to a pandas DataFrame using Arrow
-result_pdf = df.select("*").toPandas()
-
-# But see also: https://stackoverflow.com/questions/37513355/converting-pandas-dataframe-into-spark-dataframe-error/56895546
-# For imposing schema to ensure pd to sparkdf works.
+# pings, vessels = parse_gdb(gdb_folders[0], bucket_in = 'ais-ship-data-raw')
