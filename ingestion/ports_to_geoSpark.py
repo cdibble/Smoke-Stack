e3efdf383312d8d9fddffffda2eb9d0f2aa3a393
cdibble <cddibble@gmail.com>
2020-01-31 01:26:02 -0800

    ports_to_geoSpark v2 is successful. 27 second execute compared to 1.7 min from v1. The geoPoint parser now will be deployed on the ping data set.

--- ingestion/ports_to_geoSpark.py
+++ ingestion/ports_to_geoSpark.py
@@ -32,7 +32,8 @@ def get_meth(object):
 	return object_methods
 def geoPoint_buffer_to_polygon(point):
 	# this function owes a lot to 'bugmenot123': # https://gis.stackexchange.com/questions/268250/generating-polygon-representing-rough-100km-circle-around-latitude-longitude-poi/268277#268277
-	# point = shapely.wkt.loads(point) # if you want to accept string as input
+	# needs to be able to take a list of points
+	point = shapely.wkt.loads(point) # if you want to accept string as input
 	try:
 		point.__getattribute__('x')
 	except AttributeError:
@@ -55,80 +56,52 @@ def geoPoint_buffer_to_polygon(point):
 	point_aeqd = transform(wgs84_to_aeqd, point)
 	aeqd_buffered_to_polygon = point_aeqd.buffer(20_000)
 	polygon_wgs84 = transform(aeqd_to_wgs84, aeqd_buffered_to_polygon)
+	# print(type(polygon_wgs84.wkt))
 	return(polygon_wgs84.wkt)
 	# print(mapping(polygon_wgs84))
 	# print(json.dumps(mapping(buffer_wgs84)))
 ## Test geoPoint_buffer_to_polygon
 # geoPoint_buffer_to_polygon(list(geoPorts.select('LON_LAT').first())[0])
+# Register UDF
+geoPoint_buffer_to_polygon_udf = psql.udf(geoPoint_buffer_to_polygon, StringType())
 
 def lonLatString_to_geoPoint(lon, lat):
-	 # sqlContext.createDataFrame([R(i, x) for i, x in enumerate(geoPoints_wkt)])
 	points = Point(float(lon), float(lat)).wkt
 	return points
-
+# Register UDF
 lonLatString_to_geoPoint_udf = psql.udf(lonLatString_to_geoPoint)
-
-# map(lambda x: write_csv_parquet_to_s3(parse_csv(x, bucket_in = 'ais-ship-data-raw').collect(), file_name = 'pings_from_csv.parquet'))
 
 def write_df_to_parquet(spark, df_i):
 		# Write Parquet to S3 
 		target_bucket_dir = "s3a://major-us-ports-csv/"
-		target_file_name = 'geoPorts.parquet'
+		target_file_name = 'geoPorts_v2.parquet'
 		df_i.write.mode("append").format('parquet').option('compression', 'snappy').save(target_bucket_dir + target_file_name)
 ## Get Data -> Munge Spatial Points -> Add Polygon with Buffer 
 # client.download_file('BUCKET_NAME', 'OBJECT_NAME', 'FILE_NAME')
 ports = sqlContext.read.csv("s3a://major-us-ports-csv/" + 'Major_Ports.csv', header = 'True')
-lons = [float(x[0]) for x in ports.select('X').collect()] # get lons as list of floats
-lats = [float(x[0]) for x in ports.select('Y').collect()] # get lats as list of floats
-gePorts = ports.withColumn('LON_LAT', lonLatString_to_geoPoint_udf(ports.X, ports.Y))
-geoPoints = [Point(x, y) for x, y in zip(lons, lats)] # turn lons, lats into Point()
-geoPolys_wkt = [geoPoint_buffer_to_polygon(point) for point in geoPoints]
-geoPoints_wkt = [geoPoints[i].wkt for i in range(len(geoPoints))]
-# spark DataFrame version (use this to avoid conversion to RDD and back)
-# Convert geoPoints to data frame with index column
-R = Row('index', 'LON_LAT')
-spark_geoPoints = sqlContext.createDataFrame([R(i, x) for i, x in enumerate(geoPoints_wkt)])
-# Convert geoPoly to data frame with index column
-R = Row('index', 'POLYGON10KM')
-spark_geoPolys = sqlContext.createDataFrame([R(i, x) for i, x in enumerate(geoPolys_wkt)])
-# geoPoints_index = index.zip(spark_geoPoints)
-ports_index = ports.withColumn('index', psql.monotonically_increasing_id())
-# Join ports_index and geoPoints_index using index
-geoPorts = ports_index.join(spark_geoPoints, "index")
-polyPorts = geoPorts.join(spark_geoPolys, "index")
+# Version 2.
+geoPorts = ports.withColumn('LON_LAT', lonLatString_to_geoPoint_udf(ports.X, ports.Y))
+polyPorts = geoPorts.withColumn('POLYGON10KM', geoPoint_buffer_to_polygon_udf(geoPorts.LON_LAT))
+
+
+# Version 1. Slow.
+# lons = [float(x[0]) for x in ports.select('X').collect()] # get lons as list of floats
+# lats = [float(x[0]) for x in ports.select('Y').collect()] # get lats as list of floats
+# geoPoints = [Point(x, y) for x, y in zip(lons, lats)] # turn lons, lats into Point()
+# geoPolys_wkt = [geoPoint_buffer_to_polygon(point) for point in geoPoints]
+# geoPoints_wkt = [geoPoints[i].wkt for i in range(len(geoPoints))]
+# # spark DataFrame version (use this to avoid conversion to RDD and back)
+# # Convert geoPoints to data frame with index column
+# R = Row('index', 'LON_LAT')
+# spark_geoPoints = sqlContext.createDataFrame([R(i, x) for i, x in enumerate(geoPoints_wkt)])
+# # Convert geoPoly to data frame with index column
+# R = Row('index', 'POLYGON10KM')
+# spark_geoPolys = sqlContext.createDataFrame([R(i, x) for i, x in enumerate(geoPolys_wkt)])
+# # geoPoints_index = index.zip(spark_geoPoints)
+# ports_index = ports.withColumn('index', psql.monotonically_increasing_id())
+# # Join ports_index and geoPoints_index using index
+# geoPorts = ports_index.join(spark_geoPoints, "index")
+# polyPorts = geoPorts.join(spark_geoPolys, "index")
 
 ## Write to Parquet
 write_df_to_parquet(sc, polyPorts)
-
-
-
-
-
-[POINT (-90.6590509998076 29.57907000004379),
-POINT (-90.61793999968185 30.0334500003297),
-POINT (-91.19934000015607 30.42292000007941),
-POINT (-91.43073200003357 31.54723799990148),
-POINT (-90.90055999978968 32.33469999989674),
-POINT (-91.15361000028163 32.79471999991519),
-POINT (-91.12686900031129 33.35923199977935),
-POINT (-89.68744700010141 29.48000399978501),
-POINT (-90.0852559999848 29.91414000013753),
-POINT (-89.08533000044253 30.35215999978056),
-POINT (-88.55879000028344 30.34802000019582),
-POINT (-88.0411300003198 30.72527000000661),
-POINT (-84.19926000026548 30.19009000021664),
-POINT (-82.60270999960073 27.65999999979972),
-POINT (-82.52235000032071 27.7853400001281),
-POINT (-80.18164199992312 25.78286200018114),
-POINT (-80.11780100002852 26.09339200007157),
-POINT (-80.05266999962299 26.76903999983693),
-POINT (-80.60815000010946 28.41409000006081),
-POINT (-81.66512500027817 30.32275699981061),
-POINT (-81.49987999988412 31.158559999745),
-POINT (-81.0953819997664 32.08471100021607),
-POINT (-79.92159500012362 32.78878099996054),
-POINT (-95.74001999966019 36.21820000000334),
-POINT (-94.60762000032766 39.11500000019358),
-POINT (-93.06190000040833 44.95156999980364),
-POINT (-91.0220600004061 33.81355100009842),
-POINT (-90.58322999991033 34.52186000036581)] 
--- ingestion/spark_submit_extraction_.sh
+++ ingestion/spark_submit_extraction_.sh
@@ -14,10 +14,12 @@
 #   [application-arguments]
 ###
 # PREREQUISITE: From local machine, push code to Master
+# ssh -i "Connor-Dibble-IAM-keypair.pem" ubuntu@ec2-44-229-205-147.us-west-2.compute.amazonaws.com # Spark Master
 # scp -i "Connor-Dibble-IAM-keypair.pem" /Users/Connor/Documents/Graduate\ School/Dibble_Research/Github_repos/ship-soot/ingestion/pySpark_DB_Extract.py ubuntu@ec2-44-229-205-147.us-west-2.compute.amazonaws.com:/home/ubuntu/Scripts/
 # scp -i "Connor-Dibble-IAM-keypair.pem" /Users/Connor/Documents/Graduate\ School/Dibble_Research/Github_repos/ship-soot/ingestion/spark_submit_extraction_.sh ubuntu@ec2-44-229-205-147.us-west-2.compute.amazonaws.com:/home/ubuntu/Scripts/
 # From master machine, start cluster
 # sh /usr/local/spark/sbin/start-all.sh # re-run this after adding a new worker IP to the slaves file if scaling horizontally
+# /usr/local/spark/sbin/stop-all.sh # stop spark cluster
 
 # submit job to spark cluster
 /usr/local/spark/bin/spark-submit --master spark://10.0.0.7:7077 --jars /usr/local/spark/jars/aws-java-sdk-1.7.4.jar,/usr/local/spark/jars/hadoop-aws-2.7.1.jar, /home/ubuntu/Scripts/pySpark_DB_Extract.py &> /usr/local/spark/logs/spark_run_log_`date '+%Y_%m_%d__%H_%M_%S'`_terminal
