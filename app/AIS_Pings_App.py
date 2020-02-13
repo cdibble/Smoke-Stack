60f27b117256304961f7b316d39372e949264c39
cdibble <cddibble@gmail.com>
2020-02-13 10:59:06 -0800

    app to port 80

--- app/AIS_Pings_App.py
+++ app/AIS_Pings_App.py
@@ -15,6 +15,9 @@ app = Flask(__name__)
 api = Api(app)
 # run app with self-call
 # if __name__ == '__main__':
+if __name__ == "__main__":
+    app.run(port="80", host="0.0.0.0")
+
 
 def get_db():
 		# use first for development (one month of data)
@@ -264,70 +267,3 @@ def api_get_ship_visits_quarterlyPerPort(ship):
 @app.route('/smokestackAPI/v1.0/ship_query_totalTimePerPort/<ship>', methods=['GET'])
 def api_get_ship_totalTimePerPort(ship):
 	return jsonify(aggregate_ship_visits_total_time(ship))
-
-# @app.route('/port_index')
-# def port_index():
-# 	connection = psycopg2.connect("dbname=pings_db_withVC user=db_user password=look_at_data host=44.232.197.79 port=5432")
-# 	cursor = connection.cursor()
-# 	cursor.execute('''SELECT DISTINCT "PORT_NAME" FROM pings_db_withVC ''')
-# 	port_list = cursor.fetchall()
-# 	port_list = sorted(port_list)
-# 	return render_template("port_index.html", port_list=port_list)
-# # API 
-# @app.route('/portQuery/<PORT_NAME>')
-# def get_port(portName):
-
-# Route that accepts a username(string) and returns it. You can use something similar
-#   to fetch details of a username
-# @app.route('/user/<username>')
-# def show_user_profile(username):
-# 	# show the user profile for that user
-# 	return 'User %s' % escape(username)
-
-# @app.route('/userwithslash/<username>/')
-# def show_user_profile_withslash(username):
-# 	# show the user profile for that user
-# 	return 'User %s' % escape(username)
-
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-# 	# show the post with the given id, the id is an integer
-# 	return 'Post %d' % post_id
-
-# @app.route('/path/<path:subpath>')
-# def show_subpath(subpath):
-# 	# show the subpath after /path/
-# 	return 'Subpath %s' % escape(subpath)
-
-# Classes
-# class BaseModel(db.Model):
-#     """Base data model for all objects"""
-#     __abstract__ = True
-
-#     def __init__(self, *args):
-#         super().__init__(*args)
-
-#     def __repr__(self):
-#         """Define a base way to print models"""
-#         return '%s(%s)' % (self.__class__.__name__, {
-#             column: value
-#             for column, value in self._to_dict().items()
-#         })
-
-#     def json(self):
-#         """
-#                 Define a base way to jsonify models, dealing with datetime objects
-#         """
-#         return {
-#             column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
-#             for column, value in self._to_dict().items()
-#         }
-
-
-# class Station(BaseModel, db.Model):
-#     """Model for the stations table"""
-#     __tablename__ = 'stations'
-
-#     id = db.Column(db.Integer, primary_key = True)
-#     lat = db.Column(db.Float)
-#     lng = db.Column(db.Float)
